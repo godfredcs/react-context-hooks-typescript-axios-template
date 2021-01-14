1d4b89ea1ba59fd7d24f9dbfbee84bdd073f1f16
Godfred Boateng Addai <addaiz.godfred@gmail.com>
2021-01-14 00:36:31 +0000

    Added Users.tsx and installed axios and moment dependencies

---
+++ src/containers/Users/pages/Users.tsx
@@ -0,0 +1,12 @@
+import './../assets/css/Users.css';
+import React from 'react';
+
+const Users: React.FC = () => {
+    return (
+        <div className="users">
+            Welcome to the users page.
+        </div>
+    );
+};
+
+export default Users;
