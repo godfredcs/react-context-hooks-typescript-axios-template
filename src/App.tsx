a41d0ab2d9a4d79a7194b3c1a4e19aeb585b05e6
Godfred Boateng Addai <addaiz.godfred@gmail.com>
2021-01-14 00:33:40 +0000

    Refactored App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,14 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import Provider from './store/Provider';
+import Users from './containers/Users/pages/Users';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App: React.FC = () => {
+    return (
+        <Provider>
+            <Users />
+        </Provider>
+    );
+};
 
 export default App;
